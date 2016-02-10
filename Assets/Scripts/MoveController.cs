467e0ede9a92ee2e711e041b02f6be27d7eab066
fururu <tikutak@gmail.com>
2016-02-10 16:55:50 +0900

    first commit

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveController : MonoBehaviour {
+
+	public float speed = 1.0f;
+	public float startPosition;
+	public float endPosition;
+
+	// Update is called once per frame
+	void Update () {
+		//毎フレームspeedの分左へ移動させる
+		transform.Translate (-1.0f * speed * Time.deltaTime, 0, 0);
+
+		//決めた座標まで行ったら移動を終わらせる
+		if (transform.position.x < endPosition) {
+			MoveEnd ();
+		}
+	}
+	void MoveEnd () {
+		//物体の座標をstartPositionに戻す
+		transform.Translate (startPosition - endPosition, 0, 0);
+
+		//同じゲームオブジェクトにアタッチされているコンポーネントにメッセージを送る
+		SendMessage ("OnMoveEnd", SendMessageOptions.DontRequireReceiver);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	Rigidbody2D objectRb;
+	public float upForce;
+	Transform objectTf;
+	
+	void Awake () {
+		objectRb = GetComponent<Rigidbody2D> ();
+		objectTf = GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	//&& ((transform.position.y - hitInfo.transform.position.y) < 0.5f)
+	void Update () {
+		RaycastHit2D hitInfo = Physics2D.Raycast (objectTf.position,Vector2.down);
+		if (Input.GetButtonDown("Fire1")) {
+			Debug.Log("OK");
+			Jump ();
+		}
+	}
+	/*void Jump (){
+		objectRb.AddForce (Vector2.up * upForce);
+		objectRb.AddForce (new Vector2(0.0f,3.0f));
+	}*/
+	void Jump () {
+		objectRb.velocity = new Vector2(0.0f, upForce);
+	}
+}	
